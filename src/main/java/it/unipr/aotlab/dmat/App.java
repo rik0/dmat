0363b7db1d512c622945d8aa9ae6af1dff8997f0
Enrico Franchi <efranchi@ce.unipr.it>
2011-10-16 16:49:38 +0200

    First commit

---
+++ src/main/java/it/unipr/aotlab/dmat/App.java
@@ -0,0 +1,13 @@
+package it.unipr.aotlab.dmat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
