8d7b8d5252ec907d07546a4ce9235b737695bf3f
Enrico Franchi <enrico.franchi@gmail.com>
2012-05-08 10:34:46 +0200

    Added test program to check if EPGM works.

---
+++ test/test_epgm.c
@@ -0,0 +1,40 @@
+/* gcc -lzmq test.c */
+
+#include <stdio.h>
+#include <zmq.h>
+
+int main () {
+  int mainrv = 0;
+
+  void *context = zmq_init (1);
+  if (context == NULL) {
+    fprintf (stderr, "Error occurred during zmq_init(): %s\n", zmq_strerror (errno));
+    mainrv = 1;
+    goto end;
+  }
+
+  void *publisher = zmq_socket (context, ZMQ_PUB);
+  if (publisher == 0) {
+    fprintf (stderr, "Error occurred during zmq_socket(): %s\n", zmq_strerror (errno));
+    mainrv = 1;
+    goto exception0;
+  }
+
+  int rv = zmq_bind (publisher, "epgm://127.0.0.1;239.255.255.255:5561");
+  if (rv != 0) {
+    fprintf (stderr, "Error occurred during zmq_bind(): %s\n", zmq_strerror (errno));
+    mainrv = 1;
+    goto exception1;
+  }
+
+  fprintf(stderr, "\n\nSUCCESS! Ignore eventual warnings, it is normal as we used the loopback device to make it simple.\n\n");
+
+  exception1:
+  zmq_close (publisher);
+
+  exception0:
+  zmq_term (context);
+
+  end:
+  return mainrv;
+}
