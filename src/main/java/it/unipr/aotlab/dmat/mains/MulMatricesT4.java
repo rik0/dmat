9ee6c096c51488668419355d2d85a9cd6ef2bd6e
Paolo Bolzoni <paolo.bolzoni1@studenti.unipr.it>
2012-06-23 17:49:07 +0200

    Added a 4 node test

--- src/main/java/it/unipr/aotlab/dmat/mains/MulMatricesT4.java
+++ src/main/java/it/unipr/aotlab/dmat/mains/MulMatricesT4.java
@@ -12,69 +12,81 @@ import it.unipr.aotlab.dmat.core.net.zeroMQ.Nodes;
 import it.unipr.aotlab.dmat.core.registers.zeroMQ.NodeWorkGroup;
 
 public class MulMatricesT4 {
+    private static final String IP = "192.168.0.2";
+
+    private static final String B_TOP = "BTop";
+    private static final String B_BOTTOM = "BRight";
+
+    private static final String B_TOP_LEFT = "BTopLeft";
+    private static final String B_TOP_RIGHT = "BTopRight";
+    private static final String B_BOTTOM_RIGHT = "BBottomRight";
+    private static final String B_BOTTOM_LEFT = "BBottomLeft";
+
+    private static final String C_TOP = "CTop";
+    private static final String C_BOTTOM = "CBottom";
+    private static final String C_TOP_LEFT = "CTopLeft";
+    private static final String C_BOTTOM_LEFT = "CBottomLeft";
+    private static final String C_TOP_RIGHT = "CTopRight";
+    private static final String C_BOTTOM_RIGHT = "CBottomRight";
+
+
     public static void main(String[] argv) {
         NodeWorkGroup register = NodeWorkGroup.builder().
                 masterId("master").
-                masterAddress(new IPAddress("42.191.37.74", 5672)).build();
+                masterAddress(new IPAddress(IP, 5672)).build();
 
        try {
             Nodes nodes = new Nodes(register);
 
             Node testNode0 = nodes.setNodeName("testnode0")
-                    .setNodeAddress(new IPAddress("42.191.37.74", 6000)).build();
-            
+                    .setNodeAddress(new IPAddress(IP, 6000)).build();
+
             Node testNode1 = nodes.setNodeName("testnode1")
-                    .setNodeAddress(new IPAddress("42.191.37.74", 6001)).build();
-            
+                    .setNodeAddress(new IPAddress(IP, 6001)).build();
+
             Node testNode2 = nodes.setNodeName("testnode2")
-                    .setNodeAddress(new IPAddress("42.191.37.74", 6002)).build();
-            
+                    .setNodeAddress(new IPAddress(IP, 6002)).build();
+
             Node testNode3 = nodes.setNodeName("testnode3")
-                    .setNodeAddress(new IPAddress("42.191.37.74", 6003)).build();
+                    .setNodeAddress(new IPAddress(IP, 6003)).build();
 
             register.initialize();
 
             Matrix A = Matrices.newBuilder()
                     .setName("A")
-                    .setNofRows(1000)
-                    .setNofColumns(1000)
-                    .splitVerticallyChuck(null, 500, "Aleft", "Aright")
-                    .splitHorizzontalyChuck("Aleft", 500, "ATopLeft", "ATopRight")
-                    .splitHorizzontalyChuck("Aright", 500, "ABottomLeft", "ABottomRight")
+                    .setNofRows(400)
+                    .setNofColumns(400)
                     .setElementType(TypeWire.ElementType.INT32).build();
 
             Matrix B = Matrices.newBuilder()
                     .setName("B")
-                    .setNofRows(1000)
-                    .setNofColumns(1000)
-                    .splitVerticallyChuck(null, 500, "Bleft", "Bright")
-                    .splitHorizzontalyChuck("Bleft", 500, "BTopLeft", "BTopRight")
-                    .splitHorizzontalyChuck("Bright", 500, "BBottomLeft", "BBottomRight")
+                    .setNofRows(400)
+                    .setNofColumns(400)
+                    .splitHorizzontalyChuck(null, 200, B_TOP, B_BOTTOM)
+                    .splitVerticallyChuck(B_BOTTOM, 200, B_BOTTOM_LEFT, B_BOTTOM_RIGHT)
+                    .splitVerticallyChuck(B_TOP, 200, B_TOP_LEFT, B_TOP_RIGHT)
                     .setElementType(TypeWire.ElementType.INT32).build();
 
             Matrix C = Matrices.newBuilder()
                     .setName("C")
-                    .setNofRows(1000)
-                    .setNofColumns(1000)
-                    .splitVerticallyChuck(null, 500, "Cleft", "Cright")
-                    .splitHorizzontalyChuck("Cleft", 500, "CTopLeft", "CTopRight")
-                    .splitHorizzontalyChuck("Cright", 500, "CBottomLeft", "CBottomRight")
+                    .setNofRows(400)
+                    .setNofColumns(400)
+                    .splitHorizzontalyChuck(null, 200, C_TOP, C_BOTTOM)
+                    .splitVerticallyChuck(C_BOTTOM, 200, C_BOTTOM_LEFT, C_BOTTOM_RIGHT)
+                    .splitVerticallyChuck(C_TOP, 200, C_TOP_LEFT, C_TOP_RIGHT)
                     .setElementType(TypeWire.ElementType.INT32).build();
 
-            A.getChunk("ATopLeft").assignChunkToNode(testNode0);
-            A.getChunk("ATopRight").assignChunkToNode(testNode1);
-            A.getChunk("ABottomLeft").assignChunkToNode(testNode2);
-            A.getChunk("ABottomRight").assignChunkToNode(testNode3);
+            A.getChunk(null).assignChunkToNode(testNode0);
 
-            B.getChunk("BTopLeft").assignChunkToNode(testNode0);
-            B.getChunk("BTopRight").assignChunkToNode(testNode1);
-            B.getChunk("BBottomLeft").assignChunkToNode(testNode2);
-            B.getChunk("BBottomRight").assignChunkToNode(testNode3);
-            
-            C.getChunk("CTopLeft").assignChunkToNode(testNode0);
-            C.getChunk("CTopRight").assignChunkToNode(testNode1);
-            C.getChunk("CBottomLeft").assignChunkToNode(testNode2);
-            C.getChunk("CBottomRight").assignChunkToNode(testNode3);
+            B.getChunk(B_TOP_LEFT).assignChunkToNode(testNode0);
+            B.getChunk(B_TOP_RIGHT).assignChunkToNode(testNode1);
+            B.getChunk(B_BOTTOM_LEFT).assignChunkToNode(testNode2);
+            B.getChunk(B_BOTTOM_RIGHT).assignChunkToNode(testNode3);
+
+            C.getChunk(C_TOP_LEFT).assignChunkToNode(testNode1);
+            C.getChunk(C_TOP_RIGHT).assignChunkToNode(testNode2);
+            C.getChunk(C_BOTTOM_LEFT).assignChunkToNode(testNode3);
+            C.getChunk(C_BOTTOM_RIGHT).assignChunkToNode(testNode0);
 
             OrderSetMatrixBody.Builder b = OrderSetMatrixBody.newBuilder();
             b.setURI("file://" + System.getProperty("user.dir") + "/example_matrices/large");
